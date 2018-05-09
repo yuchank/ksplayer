141c6b1b92205e79b02faefd09f03a32f497b7b6
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-05-09 10:39:33 +0900

    capture

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,31 @@ extern "C" {
   #include <libswscale/swscale.h>
 }
 
-#include <cstdio>
+int decode(AVCodecContext *avctx, AVFrame *frame, int *got_frame, AVPacket *pkt);
+void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame);
 
 int main(int argc, char *argv[]) {
 
   if (argc < 2) {
-    av_log(nullptr, AV_LOG_INFO, "please provide a movie file\n");
+    av_log(nullptr, AV_LOG_INFO, "please provide a movie file.");
     exit(-1);
   }
-
-  AVFormatContext *pFormatCtx = nullptr;
 
   // register all formats and codecs
   avdevice_register_all();
 
-  // open a video file
+  // do global initialization of network components.
+  avformat_network_init();
+
+  // open video file
+  AVFormatContext *pFormatCtx = nullptr;
+
   if (avformat_open_input(&pFormatCtx, argv[1], nullptr, nullptr) != 0) {
     exit(-1);
   }
 
   // retrieve stream information
+  // populate pFormatCtx->streams
   if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
     exit(-1);
   }
@@ -31,85 +36,128 @@ int main(int argc, char *argv[]) {
   // dump information about file onto standard error
   av_dump_format(pFormatCtx, 0, argv[1], 0);
 
-  int videoStream = -1;
-
   // find the first video stream
+  int videoStream = -1;
   for (int i = 0; i < pFormatCtx->nb_streams; i++) {
     if (pFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
       videoStream = i;
       break;
     }
   }
-
   if (videoStream == -1) {
-    // didn't find a video stream
     exit(-1);
   }
 
-  AVCodec *pCodec = nullptr;
-  // find the decoder for the video stream
-  pCodec = avcodec_find_decoder(pFormatCtx->streams[videoStream]->codecpar->codec_id);
-  if (pCodec == nullptr) {
-    fprintf(stderr, "Unsupported codec\n");
-    exit(-1);  // codec not found
-  }
-
-  AVCodecContext *pCodecCtx = nullptr;
+  int width = pFormatCtx->streams[videoStream]->codecpar->width;
+  int height = pFormatCtx->streams[videoStream]->codecpar->height;
 
   // get a pointer to the codec context for the video stream
-  pCodecCtx = avcodec_alloc_context3(pCodec);
+  AVCodec *pVideoCodec = avcodec_find_decoder(pFormatCtx->streams[videoStream]->codecpar->codec_id);
+  if (pVideoCodec == nullptr) {
+    fprintf(stderr, "Unsupported codec\n");
+    exit(-1);
+  }
+  AVCodecContext *pVCtx = avcodec_alloc_context3(pVideoCodec);
 
-  /* For some codecs, such as msmpeg4 and mpeg4, width and height
-   * MUST be initialized there because this information is not
-   * available in the bitstream.
-   * */
-  pCodecCtx->width = pFormatCtx->streams[videoStream]->codecpar->width;
-  pCodecCtx->height = pFormatCtx->streams[videoStream]->codecpar->height;
-
-  if (avcodec_open2(pCodecCtx, pCodec, nullptr) < 0) {
+  // open code
+  if (avcodec_open2(pVCtx, pVideoCodec, nullptr) < 0) {
     exit(-1);
   }
 
-  AVFrame *pFrame = nullptr;
   // allocate video frame
-  pFrame = av_frame_alloc();
-
-  AVFrame *pFrameRGB = nullptr;
-  // allocate an AVFrame structure
-  pFrameRGB = av_frame_alloc();
-  if (pFrameRGB == nullptr) {
+  AVFrame *pVFrame = av_frame_alloc();
+  // allocate RGB frame
+  AVFrame *pRGBFrame = av_frame_alloc();
+  if (pVFrame == nullptr || pRGBFrame == nullptr) {
     exit(-1);
   }
 
-  int numBytes;
   // determine required buffer size and allocate buffer
-  numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height, 32); // 32 bytes
-
-  uint8_t *buffer = nullptr;
-  buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
+  int numBytes = av_image_get_buffer_size(AV_PIX_FMT_RGB24, width, height, 1);
+  auto *buffer = (uint8_t *)av_malloc(numBytes * sizeof(uint8_t));
 
   // assign appropriate parts of buffer to image planes in pFrameRGB
   // Note that pFrameRGB is an AVFrame, but AVFrame is a superset of AVPicture
-//  uint8_t *dst_data[4];
-//  int dst_line_size[4];
-//  av_image_fill_arrays(dst_data, dst_line_size, buffer, AV_PIX_FMT_RGB24, pCodecCtx->width, pCodecCtx->height, 32);
+  av_image_fill_arrays(pRGBFrame->data, pRGBFrame->linesize, buffer, AV_PIX_FMT_RGB24, width, height, 1);
 
-//  struct SwsContext *sws_ctx = nullptr;
-//  // initialize SWS context for software scaling
-//  sws_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, nullptr, nullptr, nullptr);
+  // initialize SWS context for software scaling
+  struct SwsContext *sws_ctx = sws_getContext(width, height, AV_PIX_FMT_YUV420P, width, height, AV_PIX_FMT_RGB24, SWS_BILINEAR, nullptr, nullptr, nullptr);
+
+  AVPacket packet;
+  int frameFinished;
+  int i = 0;
+  while (av_read_frame(pFormatCtx, &packet) >= 0) {
+    if (packet.stream_index == videoStream) {
+      decode(pVCtx, pVFrame, &frameFinished, &packet);
+
+      if (frameFinished) {
+        sws_scale(sws_ctx, (uint8_t const * const *)pVFrame->data, pVFrame->linesize, 0, height, pRGBFrame->data, pRGBFrame->linesize);
+        i++;
+        if (i > 10000 && i <= 10005) {
+          SaveFrame(pRGBFrame, width, height, i);
+        }
+      }
+    }
+    av_packet_unref(&packet);
+  }
 
   // free the RGB image
   av_free(buffer);
+  av_frame_free(&pRGBFrame);
 
-  // free the video frames
-  av_frame_free(&pFrame);   // RGB
-  av_frame_free(&pFrameRGB);  // YUV
+  // free the YUV frame
+  av_frame_free(&pVFrame);
 
-  // close the codecs
-  avcodec_close(pCodecCtx);
+  // close the codec
+  avcodec_close(pVCtx);
 
   // close the video file
   avformat_close_input(&pFormatCtx);
+}
+
+int decode(AVCodecContext *avctx, AVFrame *frame, int *got_frame, AVPacket *pkt)
+{
+  int ret;
+
+  *got_frame = 0;
+
+  if (pkt) {
+    ret = avcodec_send_packet(avctx, pkt);
+    // In particular, we don't expect AVERROR(EAGAIN), because we read all
+    // decoded frames with avcodec_receive_frame() until done.
+    if (ret < 0) {
+      return ret == AVERROR_EOF ? 0 : ret;
+    }
+  }
+
+  ret = avcodec_receive_frame(avctx, frame);
+  if (ret < 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF)
+    return ret;
+  if (ret >= 0)
+    *got_frame = 1;
 
   return 0;
+}
+
+void SaveFrame(AVFrame *pFrame, int width, int height, int iFrame)
+{
+  FILE *pFile;
+  char szFilename[32];
+  int  y;
+
+  // Open file
+  sprintf(szFilename, "frame%d.ppm", iFrame);
+  pFile = fopen(szFilename, "wb");
+  if (pFile == nullptr)
+    return;
+
+  // Write header
+  fprintf(pFile, "P6\n%d %d\n255\n", width, height);
+
+  // Write pixel data
+  for (y = 0; y < height; y++)
+    fwrite(pFrame->data[0]+y*pFrame->linesize[0], 1, width*3, pFile);
+
+  // Close file
+  fclose(pFile);
 }
