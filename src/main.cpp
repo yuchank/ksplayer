627da1ecdbf3f96dd98a9319834df995e57ebe05
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-05-16 21:57:00 +0900

    before parsing

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,65 @@ extern "C" {
 #include <SDL.h>
 #include <SDL_thread.h>
 
-int main(int argc, char *argv[]) {
+typedef struct _VideoState {
 
-  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window;
 
-  fprintf(stderr, "ERROR: %d\n", 3);
-  SDL_Log("sdl_log available?");
-  av_log(NULL, AV_LOG_INFO, "av_log available?");
+  SDL_Thread *parse_tid;
 
-  SDL_Window *window = SDL_CreateWindow("SDL_CreateTexture",
-                                        SDL_WINDOWPOS_UNDEFINED,
-                                        SDL_WINDOWPOS_UNDEFINED,
-                                        640,
-                                        480,
-                                        SDL_WINDOW_RESIZABLE);
+} VideoState;
 
+int parse_thread(void *arg) {
+
+}
+
+int main(int argc, char *argv[])
+{
   SDL_Event event;
-  while (1) {
+  SDL_Window *window = NULL;
+
+  VideoState *is = NULL;
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: ksplayer <file>\n");
+    exit(1);
+  }
+
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {
+    fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  is = static_cast<VideoState *>(av_mallocz(sizeof(VideoState)));
+
+  window = SDL_CreateWindow("SDL_CreateTexture", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+  if (!window) {
+    goto exit;
+  }
+  is->window = window;
+
+  is->parse_tid = SDL_CreateThread(parse_thread, "parse", is);
+  if (!is->parse_tid) {
+    goto exit;
+  }
+
+  while (true) {
     SDL_PollEvent(&event);
-    if (event.type == SDL_QUIT) {
-      break;
+    switch (event.type) {
+      case SDL_QUIT:
+        goto exit;
+      default:
+        break;
     }
+  }
+
+exit:
+  if (window) {
+    SDL_DestroyWindow(window);
+  }
+
+  if (is) {
+    av_free(is);
   }
 
   SDL_Quit();
